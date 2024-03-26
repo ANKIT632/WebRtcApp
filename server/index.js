fa14d0489fcd15a1c2f1141110e06aa42135061e
ankit gupta <ankitgupta17878@gmail.com>
2024-03-26 14:44:16 +0530

    -

--- server/index.js
+++ server/index.js
@@ -5,6 +5,32 @@ const io =new Server(8000,{
   cors:'true',
 });
 
-io.on('connection',(socket)=>{
-  console.log('socket',socket.id);
+const emailToSocketIdMap=new Map();
+const socketIdToEmailMap=new Map();
+
+io.on('connection',(socket)=>{ 
+  socket.on('room-join',(data)=>{
+  const {email,room}=data;
+  emailToSocketIdMap.set(email,socket.id);
+  socketIdToEmailMap.set(socket.id,email);
+  
+// if user join the room.
+  io.to(room).emit('user-join',{email,id:socket.id});
+  socket.join(room);
+
+  io.to(socket.id).emit('room-join',data);
+  });
+
+  socket.on('user-call',({to,offer})=>{
+    io.to(to).emit("incomming-call",{from:socket.id,offer});
+  });
+
+  socket.on('call-accept',({to,ans})=>{
+    io.to(to).emit("call-accept",{from:socket.id,ans});
+  });
+
 });
+
+
+
+// here user-join and room-join are event listner which is same both side client as well as sever .
